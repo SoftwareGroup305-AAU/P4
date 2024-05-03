d3a5b3a990e64528301bd7cdb7c75813a9238360
Emil <emilsprojects@gmail.com>
2024-05-03 14:42:52 +0200

    Update Program.cs

--- P4.TinyCell/Program.cs
+++ P4.TinyCell/Program.cs
@@ -17,7 +17,11 @@ internal class Program
 
         var tokenStream = new CommonTokenStream(lexer);
 
+        var ParserHelper = new ParserHelper();
+
         var parser = new TinyCellParser(tokenStream);
+
+        parser.AddErrorListener(new ParserHelper.NoErrorListener());
 
         var tree = parser.document();
         tokenStream.Fill();
@@ -58,7 +62,7 @@ internal class Program
 
         Console.WriteLine("\n=================================================\n");
         Console.WriteLine("Parse Tree:");
-        var ParserHelper = new ParserHelper();
+
         ParserHelper.PrintTree(tree);
 
         AstBuilderVisitor astBuilderVisitor = new();
