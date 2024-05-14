2193332f388645330a84501bd799f6b49cf84a83
Emilprivate <emilsprojects@gmail.com>
2024-05-14 18:38:02 +0200

    Added a new upcoming typechecker test

---
+++ P4.TinyCell.Tests/UnitTests/Typechecker/TypecheckerErrorHandling.cs
@@ -0,0 +1,13 @@
+﻿using static P4.TinyCell.Tests.Utility;
+using P4.TinyCell.Shared.Language.Typechecking;
+using Antlr4.Runtime;
+using P4.TinyCell.Shared.Utilities;
+using P4.TinyCell.Shared.Language.AbstractSyntaxTree;
+using System.ComponentModel;
+
+namespace P4.TinyCell.Tests.UnitTests.Typechecker;
+
+public class TypecheckerErrorHandling
+{
+
+}
