d1c08be1fc0ad57bf61a617134c63274232ca6df
Benjamin Høj <44369126+Benj1x@users.noreply.github.com>
2024-05-16 13:37:15 +0200

    fix for include

--- P4.TinyCell/Arduino/Arduino.ino
+++ P4.TinyCell/Arduino/Arduino.ino
@@ -1,16 +1,32 @@
-#include <Arduino.h>
+#include <Arduino.h> 
 
-byte led = 6;
-void setup()
-{
-	Serial.begin(9600);
-	pinMode(led, OUTPUT);
-}
-void loop()
-{
-	Serial.println("nydelig dame");
-	digitalWrite(led, HIGH);
-	delay(500);
-	digitalWrite(led, LOW);
-	delay(500);
-}
+int num = -(-(2))- -(-(-(2)));
+int func(int nums[], int num) {
+	num = nums[1];
+	return num;;}
+int func2(int num) {
+	int num2 = 2;;
+	return num;;}
+void setup() {
+	int d[5];
+	int e[5] = {1, 2, 3, 4};
+	d[0] = 1;
+	d[2] = e[0];
+	int i = 2;;
+	d[i] = 1;
+	e[4] = 4, 4, 2;
+	if(d[0] == 1) {;};
+	while(1) {
+	int num2 = 2;;
+	break;;};
+	func(d, i);
+	func2(d[0]);}
+void loop() {
+	Serial.println("fuck");
+	byte led = 6;;
+	byte pines = 1;;
+	bool yes = true;;
+	bool no;;
+	digitalWrite(led, true);
+	no = digitalRead(led);
+	digitalWrite(led, yes);}
